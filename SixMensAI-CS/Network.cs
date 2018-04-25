d9bf021ad2c4eefe8aaa8acbb0ea2ef5fae7f2d4
elph888 <mgeer@uco.edu>
2018-04-25 17:25:47 -0500

    added network files

--- SixMensAI-CS/Network.cs
+++ SixMensAI-CS/Network.cs
@@ -3,33 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
 
 namespace SixMensAI_CS
 {
 
     class Network
     {
-
-        String IP;
+        #region private members
+        private TcpListener Listener;
+        private Thread ListenThread;
+        private TcpClient connectedClient;
+        #endregion
+        public string outMove;
 
         public void Host()
         {
+            ListenThread = new Thread(new ThreadStart(ListenRequest));
+            ListenThread.IsBackground = true;
+            ListenThread.Start();
+        }
+        private void ListenRequest()
+        {
+            try
+            {
+                Listener = new TcpListener(52018);
+                Listener.Start();
+                Console.WriteLine("Listening at port 52018...");
+                Byte[] bytes = new Byte[1024];
+                while (true)
+                {
+                    using (connectedClient = Listener.AcceptTcpClient())
+                    {
+                        Console.WriteLine("Client Connected");
+                        using (NetworkStream stream = connectedClient.GetStream())
+                        {
+                            int length;
+                            while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                            {
+                                Console.WriteLine("Reading in Opponent move.");
+                                var incommingBytes = new byte[length];
+                                Array.Copy(bytes, 0, incommingBytes, 0, length);
+                                string incommingMove = Encoding.ASCII.GetString(incommingBytes);
+                                Console.WriteLine("Move recieved " + incommingMove);
 
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (SocketException sockerErr)
+            {
+                Console.WriteLine("SocketException: " + sockerErr);
+            }
         }
 
-        public void Connect(String inputIP)
+        private void SendMessage()
         {
-            IP = inputIP;
-        }
+            if (connectedClient == null)
+            {
+                Console.WriteLine("No client connected at the moment.");
+                return;
+            }
 
-        internal void Send(ref int start, ref int destination)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal void Receive(ref int start, ref int destination)
-        {
-            throw new NotImplementedException();
+            try
+            {
+                NetworkStream stream = connectedClient.GetStream();
+                if (stream.CanWrite)
+                {
+                    byte[] outBytes = Encoding.ASCII.GetBytes(outMove);
+                    stream.Write(outBytes, 0, outBytes.Length);
+                    Console.WriteLine("Server sent move" + outMove);
+                }
+            }
+            catch (SocketException socketErr)
+            {
+                Console.WriteLine("Socket Exception: " + socketErr);
+            }
         }
     }
 }
+
---
+++ SixMensAI-CS/NeworkClient.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace SixMensAI_CS
+{
+    class NetworkClient
+    {
+        #region private members
+        Thread inThread;
+        TcpClient connection;
+        String serverIP;
+        String outMove;
+        #endregion
+        public void ConnectServer(String input)
+        {
+            serverIP = input;
+            try
+            {
+                inThread = new Thread(new ThreadStart(Listen));
+                inThread.IsBackground = true;
+                inThread.Start();
+                Console.WriteLine("Thread is listening");
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Client connection exception " + err);
+            }
+        }
+        private void Listen()
+        {
+            try
+            {
+                connection = new TcpClient("serverIP", 52018);
+                Byte[] bytes = new Byte[1024];
+                while (true)
+                {
+                    using (NetworkStream stream = connection.GetStream())
+                    {
+                        int length;
+                        while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var incommingBytes = new byte[length];
+                            Array.Copy(bytes, 0, incommingBytes, 0, length);
+                            string incommingMessage = Encoding.ASCII.GetString(incommingBytes);
+                            Console.WriteLine("Incomming Move is " + incommingMessage);
+                        }
+                    }
+
+                }
+            }
+            catch (SocketException socketErr)
+            {
+                Console.WriteLine("Socket Exception: " + socketErr);
+            }
+        }
+        private void SendMessage()
+        {
+            if (connection == null)
+            {
+                Console.WriteLine("Not connected to a server...");
+                return;
+            }
+            try
+            {
+                NetworkStream stream = connection.GetStream();
+                if (stream.CanWrite)
+                {
+                    string outMessage = outMove;
+                    byte[] outBytes = Encoding.ASCII.GetBytes(outMessage);
+                    stream.Write(outBytes, 0, outBytes.Length);
+                    Console.WriteLine("Our move has been sent to the server.");
+                }
+            }
+            catch (SocketException socketErr)
+            {
+                Console.WriteLine("Socket Exception: " + socketErr);
+            }
+        }
+
+    }
+
+}
+}
